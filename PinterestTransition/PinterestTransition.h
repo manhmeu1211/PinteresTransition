985a1f9e62c5b67aff0240d969111c1d148730b7
sondd <sondd>
2024-06-06 10:29:16 +0700

    create pod transition

---
+++ PinterestTransition/PinterestTransition.h
@@ -0,0 +1,18 @@
+//
+//  PinterestTransition.h
+//  PinterestTransition
+//
+//  Created by đào sơn on 06/06/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PinterestTransition.
+FOUNDATION_EXPORT double PinterestTransitionVersionNumber;
+
+//! Project version string for PinterestTransition.
+FOUNDATION_EXPORT const unsigned char PinterestTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PinterestTransition/PublicHeader.h>
+
+
